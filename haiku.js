7745b612903752ff3354de4e8b2845cdb489a9da
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-05-31 03:54:28 -0400

    a lot of progress

--- haiku.js
+++ haiku.js
@@ -37,6 +37,44 @@ function formatData(data){
   return syllablesArr;  
 }
 
+function formatData2(data){ 
+   var syllDict={};
+   var lines = data.toString().split("\n"),
+       lineSplit
+   lines.forEach(function(line){  
+	    lineSplit = line.split("  "); 
+	    if(lineSplit[1]!=undefined){ 
+		    var phonemes=lineSplit[1].split(" ");
+		    var word=lineSplit[0];
+		    var syllCount=0; // 
+		    for(var i=0;i<phonemes.length;i++){
+			    if(phonemes[i].match(/\d/)){
+			    		syllCount++;
+			    }
+			 }
+		}
+	    syllDict[word]=syllCount;
+  }); 
+  //console.log(syllDict);
+  return syllDict; 
+}
+
+/*
+function getSyllDict(syllablesArr){
+	var syllDict={};
+	for(var i=0;i<syllablesArr.length;i++){
+		//console.log("working1");
+		if(syllablesArr[i]!=undefined){
+			//console.log("working2");
+			for(var j=0;j<syllablesArr[i].length;j++){
+				//console.log("working3");
+				syllDict[syllablesArr[i][j]]=i;
+			}
+		}
+	}
+	return syllDict;
+}
+*/
 function generateFrequencyTable(text){
 	freqTable={};
 	var arr=text.split(" ");
@@ -55,31 +93,76 @@ function generateFrequencyTable(text){
 	return freqTable;
 }
 
+function processText(file){
+	return fs.readFileSync(file).toString().replace(/[^a-zA-Z!' ]+/g, "").toUpperCase();
+}
+
 function haikuMarkov(structure){
-	var text="WHAT I THOUGHT WERE FACES ARE PLUMES OF PAMPAS GRASS. YOSA BUSON THREE EXAMPLES OF THE HAIKU OF YOSA BUSON FROM THE LATE 1700S ARE OFFERED HERE: A SUMMER RIVER BEING CROSSED HOW PLEASING WITH SANDALS IN MY HANDS! LIGHT OF THE MOON MOVES WEST, FLOWERS' SHADOWS CREEP EASTWARD. IN THE MOONLIGHT, THE COLOR AND SCENT OF THE WISTERIA SEEMS FAR AWAY. KOBAYASKI ISSA HERE ARE THREE HAIKU FROM KOBAYASHI ISSA, A HAIKU MASTER POET FROM THE LATE 1700S AND EARLY 1800S: O SNAIL CLIMB MOUNT FUJI, BUT SLOWLY, SLOWLY! TRUSTING THE BUDDHA, GOOD AND BAD, I BID FAREWELL TO THE DEPARTING YEAR. EVERYTHING I TOUCH WITH TENDERNESS, ALAS, PRICKS LIKE A BRAMBLE. NATSUME SOSEKI NATSUME SOSEKI LIVED FROM 1867 - 1916. HE WAS A NOVELIST AND MASTER OF THE HAIKU. HERE ARE A COUPLE OF EXAMPLES OF HIS POEMS: OVER THE WINTRY FOREST, WINDS HOWL IN RAGE WITH NO LEAVES TO BLOW. THE CROW HAS FLOWN AWAY: SWAYING IN THE EVENING SUN, A LEAFLESS TREE. RECENT POEMS FOLLOWING ARE SOME RECENT EXAMPLES OF HAIKU POEMS: IN THE COOLNESS OF THE EMPTY SIXTH-MONTH SKY... THE CUCKOO'S CRY. - MASAOKA SHIKI WHITECAPS ON THE BAY: A BROKEN SIGNBOARD BANGING IN THE APRIL WIND. - RICHARD WRIGHT LILY: OUT OF THE WATER OUT OF ITSELF - NICK VIRGILIO GROUND SQUIRREL BALANCING ITS TOMATO ON THE GARDEN FENCE - DON EULERT AS THE WIND DOES BLOW ACROSS THE TREES, I SEE THE BUDS BLOOMING IN MAY I WALK ACROSS SAND AND FIND MYSELF BLISTERING IN THE HOT, HOT HEAT FALLING TO THE GROUND, I WATCH A LEAF SETTLE DOWN IN A BED OF BROWN. IT'S COLD—AND I WAIT FOR SOMEONE TO SHELTER ME AND TAKE ME FROM HERE. I HEAR CRACKLING CRUNCH, OF TODAY'S NEW FOUND DAY AND KNOW IT WON'T LAST FALLING TO THE GROUND, I WATCH A LEAF SETTLE DOWN IN A BED OF BROWN. A CRICKET DISTURBED THE SLEEPING CHILD; ON THE PORCH A MAN SMOKED AND SMILED. I'M TURNING OVER LOOK OUT AND GIVE ME ROOM THERE YOU CRICKET, YOU. LIMERICKS AND HAIKU THERE ARE NUMEROUS TYPES OF POEMS, FROM SILLY LIMERICKS TO THE SERIOUS DRAMATIC POETRY. LIMERICKS ARE LIKE HAIKU, IN THAT THEY ARE SHORT POEMS WITH A CERTAIN NUMBER OF LINES. FOLLOWING IS AN EXAMPLE OF A FUNNY LIMERICK BY EDWARD LEAR: THERE WAS AN OLD MAN WITH A BEARD, WHO SAID, 'IT IS JUST AS I FEARED! TWO OWLS AND A HEN, FOUR LARKS AND A WREN, HAVE ALL BUILT THEIR NESTS IN MY BEARD!'";
+	var syllDict=formatData2(cmudictFile);
+	var text=processText('./basho.txt');
+	//console.log(text);
 	var freqTable=generateFrequencyTable(text);
 	var keys=Object.keys(freqTable);
-	var firstIndex=getRandomIntInclusive(0,keys.length-1)
-	var current=keys[firstIndex];
+	//console.log("about to generate");
+	var wordsUsed={};
 	var str="";
-	var counter=0;
-	while(counter<10){
-		str=str+current+" ";
-		//console.log("current",current);
-		//console.log(freqTable[current]);
-		current=selectNext(current,freqTable);
-		counter++;
+	for(var i=0;i<structure.length;i++){
+		var nextLine=getNextLine(structure[i],keys,freqTable,syllDict,wordsUsed);
+		var arr=nextLine.split(" ");
+		for(var j=0;j<arr.length;j++){
+			wordsUsed[arr[j]]=true;
+		}
+		str=str+nextLine+"\n";
+		//console.log(wordsUsed);
 	}
 	console.log(str);
+	
 
 }
-function selectNext(current,freqTable){
+
+function getNextLine(syllables,keys,freqTable,syllDict,wordsUsed){
+	//var firstIndex=getRandomIntInclusive(0,keys.length-1)
+	//var current=keys[firstIndex];	
+	while(true){
+		syllablesLeft=syllables;
+		str="";
+		var firstIndex=getRandomIntInclusive(0,keys.length-1)
+		var current=keys[firstIndex];	
+		while(syllablesLeft>0){
+			var nextWord=selectNextWord(current,freqTable,wordsUsed,keys);
+			if(syllDict[nextWord]===undefined){ 
+				var nextIndex=getRandomIntInclusive(0,keys.length-1)
+				nextWord=keys[nextIndex];
+			}
+			var syllablesUsed=syllDict[nextWord];
+			str=str+nextWord+" "
+			syllablesLeft-=syllablesUsed;
+			current=nextWord;
+			if(syllablesLeft==0&&checkEnd(str)){
+				return str;
+			}
+		}
+	}
+}
+
+function checkEnd(str){
+	var arr=str.split(" ");
+	var forbidden=["THE","A","OF","ON","BY","FROM","FOR","AND","OR","YET","TO","MY","SHE","WITH","ITS","INTO","BUT"];
+	for(var i=0;i<forbidden.length;i++){
+		if(arr[arr.length-2]==forbidden[i]){
+			//console.log("caught",str);
+			return false;
+		}
+	}
+	return true;
+}
+
+function selectNextWord(current,freqTable,wordsUsed,keys){
 	if(freqTable[current]===undefined){
 		console.log(current);
 	} //DEBUGGING
 	var successors=Object.keys(freqTable[current]);
 
-	//console.log("succcessors",successors);
 	var probRanges=[];
 	var sum=0;
 	for(var i=0;i<successors.length;i++){
@@ -97,8 +180,6 @@ function selectNext(current,freqTable){
 	var upperLim=probRanges[0];
 	var counter=0;
 	while(randNum>upperLim){
-		//console.log("   ",counter);
-		//console.log("   ",randNum,"|",upperLim);
 		if(randNum<probRanges[counter+1]){
 			counter++;
 			upperLim=probRanges[counter];
@@ -107,20 +188,27 @@ function selectNext(current,freqTable){
 			break;
 		}
 	}
-
-	//console.log("ranges",probRanges);
-	//console.log("randNum",randNum);
-	//console.log("counter",counter);
-	//console.log("successots",successors);
 	var nextWord=successors[counter];
-	//console.log(nextWord+"\n");
+	if(wordsUsed[nextWord]==true){
+		//console.log("repeat");
+		if(Math.random()>.25){
+			if(successors.length!=1){
+				//console.log("1");
+				var newIndex=getRandomIntInclusive(0,successors.length-1)
+				return successors[newIndex];
+			}
+			//console.log("2");
+			//var newIndex=getRandomIntInclusive(0,successors.length-1)
+			//return successors[newIndex];
+
+		}
+	}
 	return nextWord;
 }
 
 
 function createHaiku(structure){
 	var syllablesArr=formatData(cmudictFile);
-	//console.log(structure);
 	var complexStructure=[];
 	for(var i=0;i<structure.length;i++){
 		var availableSylls=structure[i];
@@ -132,20 +220,20 @@ function createHaiku(structure){
 		}
 		complexStructure.push(lineStructure);
 	}
-	//console.log(complexStructure);
 	var poem="\n";
 	for(var i=0;i<complexStructure.length;i++){
 		for(var j=0;j<complexStructure[i].length;j++){
 			var numSylls=complexStructure[i][j];
 			var wordIndex=getRandomIntInclusive(0,syllablesArr[numSylls].length);
-			var word=syllablesArr[numSylls][wordIndex].replace(/[^a-zA-Z!']+/g, "");
+			var word=syllablesArr[numSylls][wordIndex].replace(/[^a-zA-Z!']+/g, ""); //undefined error
 			poem=poem+word+" "
 		}
 		poem+="\n";
 	}
+	console.log("\n[basic method]");
 	console.log(poem);
-    //console.log("this should log a haiku with the structure " + structure);
-    haikuMarkov();
+	console.log("[extra credit]\n");
+    haikuMarkov(structure);
 }
 
 
