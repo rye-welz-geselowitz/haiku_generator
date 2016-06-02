22d27e2f5b9ddacc3aa9f75fabdb66ad74e2e6a8
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-06-01 23:12:17 -0400

    DONE

--- haiku.js
+++ haiku.js
@@ -1,6 +1,13 @@
+/* Three methods of haiku generation: 
+(1) Basic: Selecting random words from a dictionary
+(2) Markov chain 1: Uses Markov chain to generate likely strings of words; train on any text
+(3) Markov chain 2: Uses a backwards Markov chain to build the haiku backwards; train on other haikus
+*/
+
 var fs = require("fs");
 var cmudictFile = readCmudictFile('./cmudict.txt');
 
+//SHARED FUNCTIONS
 function readCmudictFile(file){
   return fs.readFileSync(file).toString();
 }
@@ -9,13 +16,52 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+//FUNCTIONS FOR BASIC METHOD
+
+/*Generates a random word structure fitting the syllabic demands 
+(e.g. [5,7,5] -> [[2,3],[3,3,1],[5]])
+Selects random words of the proper syllable length to fill out structure*/
+function haikuBasic(structure){
+	//Generate syllablesArr(data structure containing CMU dict parsed by syllables)
+	var syllablesArr=formatData(cmudictFile);
+	//Generate word structure (complexStructure)
+	var complexStructure=[];
+	for(var i=0;i<structure.length;i++){
+		var availableSylls=structure[i];
+		var lineStructure=[];
+		while(availableSylls>0){
+			var syllsInWord=getRandomIntInclusive(1,availableSylls);
+			while(syllablesArr[syllsInWord]==undefined){
+				syllsInWord=getRandomIntInclusive(1,availableSylls);
+			}
+			availableSylls-=syllsInWord;
+			lineStructure.push(syllsInWord);
+		}
+		complexStructure.push(lineStructure);
+	}
+	//Choose random words from syllableArr to fill out word structure
+	var poem="\n";
+	for(var i=0;i<complexStructure.length;i++){
+		for(var j=0;j<complexStructure[i].length;j++){
+			var numSylls=complexStructure[i][j];
+			var wordIndex=getRandomIntInclusive(0,syllablesArr[numSylls].length-1); 
+			var word=syllablesArr[numSylls][wordIndex].replace(/[^a-zA-Z!']+/g, ""); 
+			poem=poem+word+" "
+		}
+		poem+="\n";
+	}
+	console.log(poem);	
+}
+
+/*Parses the CMU dictionary; returns a syllable array; each index contains an array of words 
+	from the CMU dict consisting of (index) syllables.*/
 function formatData(data){ 
    var syllablesArr=[];   
    var lines = data.toString().split("\n"),
        lineSplit
    lines.forEach(function(line){  
     lineSplit = line.split("  ");    
-    //console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
     var word=lineSplit[0];
     var syllCount=0; //  
     if(lineSplit[1]!=undefined){
@@ -31,12 +77,35 @@ function formatData(data){
 	    else{
 	    	syllablesArr[syllCount].push(word);
 	    }
-	    //console.log(word,syllCount);
 	}
   }); 
   return syllablesArr;  
 }
 
+//FUNCTIONS FOR MARKOV CHAIN 1
+/*Parses a text  into a frequency table
+that maps words to the counts of words appearing after them;
+builds chains of words, in the proper syllabic structure,
+based on these frequencies*/
+function haikuMarkov(structure,text){
+	var syllDict=formatData2(cmudictFile);
+	var text=processText(text);
+	var freqTable=generateFrequencyTable(text);
+	var keys=Object.keys(freqTable);
+	var wordsUsed={};
+	var str="";
+	for(var i=0;i<structure.length;i++){
+		var nextLine=getNextLine(structure[i],keys,freqTable,syllDict,wordsUsed);
+		var arr=nextLine.split(" ");
+		for(var j=0;j<arr.length;j++){
+			wordsUsed[arr[j]]=true;
+		}
+		str=str+nextLine+"\n";
+	}
+	console.log(str);
+}
+/*Parses the CMU dictionary; returns a dictionary mapping each word 
+to its syllable count*/
 function formatData2(data){ 
    var syllDict={};
    var lines = data.toString().split("\n"),
@@ -55,26 +124,18 @@ function formatData2(data){
 		}
 	    syllDict[word]=syllCount;
   }); 
-  //console.log(syllDict);
   return syllDict; 
 }
 
-/*
-function getSyllDict(syllablesArr){
-	var syllDict={};
-	for(var i=0;i<syllablesArr.length;i++){
-		//console.log("working1");
-		if(syllablesArr[i]!=undefined){
-			//console.log("working2");
-			for(var j=0;j<syllablesArr[i].length;j++){
-				//console.log("working3");
-				syllDict[syllablesArr[i][j]]=i;
-			}
-		}
-	}
-	return syllDict;
+/*Transforms training text to uppercase and removes extraneous characters*/
+function processText(file){
+	return fs.readFileSync(file).toString().replace(/[^a-zA-Z!' ]+/g, "").toUpperCase();
 }
-*/
+
+/*Generates a frequency table for a text, mapping each word to an object that
+maps following words to their counts
+E.g. "The dog walked, the dog sat. ->
+{"the":{"dog":2},"dog":{"walked":1,"sat":1},"sat":{},"walked":{"the":1}}*/
 function generateFrequencyTable(text){
 	freqTable={};
 	var arr=text.split(" ");
@@ -92,180 +153,9 @@ function generateFrequencyTable(text){
 	}
 	return freqTable;
 }
-
-function processText(file){
-	return fs.readFileSync(file).toString().replace(/[^a-zA-Z!' ]+/g, "").toUpperCase();
-}
-
-function generateFrequencyTableNew(text){
-	freqTable={};
-	var arr=text.split("\n\n");
-	var lastWords=[];
-	for(var i=0;i<(arr.length-1);i++){
-		//COLLECT LAST WORD
-		var haiku=arr[i].split(" ");
-		var lastWord=haiku[haiku.length-1];
-		var index=haiku.length-1;
-		//console.log(index);
-		var changed=false;
-		while(lastWord==""&&index>0){
-			lastWord=haiku[index];
-			index--;
-			changed=true;
-		}
-		lastWords.push(lastWord);
-		//GENERATE BACKWARDS FREQUENCY CHAIN
-		var startIndex=index;
-		if(changed==true){
-			startIndex++;
-		}
-		var chainBack=[];
-		for(var j=startIndex;j>=0;j--){
-			var word=haiku[j];
-			if(word!=""){
-				var words=word.split('\n');
-				for(var k=words.length-1;k>=0;k--){
-					if(words[k]!=""){
-						chainBack.push(words[k]);
-					}
-				}
-			}
-		}	
-		for(var j=0;j<chainBack.length;j++){
-			if(freqTable[chainBack[j]]===undefined){
-	   			freqTable[chainBack[j]]={};
-	   		}
-	   		if(freqTable[chainBack[j]][chainBack[j+1]]===undefined){
-	   			freqTable[chainBack[j]][chainBack[j+1]]=1;
-	  		}
-	  		else{
-	   			freqTable[chainBack[j]][chainBack[j+1]]+=1;		
-	  		}
-	  	}
-	}
-    //console.log(freqTable);
-    var toReturn=[lastWords,freqTable];
-	return toReturn;
-}
-
-function processTextNew(file){
-	return fs.readFileSync(file).toString().replace(/[^a-zA-Z!' \n]+/g,"").toUpperCase();
-}
-
-function getNextLineNew(syllables,keys,freqTable,syllDict,wordsUsed,lastWord,seed){
-	var calls=0;
-	while(true){
-		if(calls>50){ //CHEATING
-			return getNextLineNew(syllables,keys,freqTable,syllDict,wordsUsed,keys[getRandomIntInclusive(0,keys.length-1)],null);
-		}
-		calls++;
-		var current=lastWord;
-		if(seed!=null){
-			current=selectNextWord(seed,freqTable,wordsUsed,keys);
-		}
-		if((current==undefined)||(syllDict[current]==undefined)){
-			current=keys[getRandomIntInclusive(0,keys.length-1)];
-		}
-		var syllablesUsed=syllDict[current];
-		var syllablesLeft=syllables-syllablesUsed;
-		var str=current;
-		if(syllablesLeft==0){
-			return str;
-		}
-		while(syllablesLeft<0){
-			current=keys[getRandomIntInclusive(0,keys.length-1)];
-			syllablesUsed=syllDict[current];
-			syllablesLeft=syllables-syllablesUsed;
-		}
-		var attempts=0;
-		while(syllablesLeft>0){
-			var nextWord=selectNextWord(current,freqTable,wordsUsed,keys);
-			if(syllDict[nextWord]==undefined){ 
-				//console.log("hey1");
-				nextWord=keys[getRandomIntInclusive(0,keys.length-1)];
-			}
-			if(attempts>=3){
-				str="";
-				syllablesLeft=syllables;
-				nextWord=keys[getRandomIntInclusive(0,keys.length-1)];
-				attempts=0;
-			}
-			syllablesUsed=syllDict[nextWord];
-			//console.log(syllablesUsed);
-			//console.log(nextWord);
-			str=nextWord+" "+str;
-			syllablesLeft-=syllablesUsed;
-			current=nextWord;
-			attempts++;
-			if(syllablesLeft==0&&checkEnd(str)){
-				return str;
-			}
-			if(syllablesLeft<0){
-				str="";
-				syllablesLeft=syllables;
-				current=keys[getRandomIntInclusive(0,keys.length-1)];
-				attempts=0;
-			}
-			//console.log(current,syllablesLeft);
-		}
-		//console.log("exited");
-	}
-}
-
-function haikuMarkovNew(structure){
-	var syllDict=formatData2(cmudictFile);
-	var text=processTextNew('./basho.txt');
-	var info=generateFrequencyTableNew(text);
-	var lastWords=info[0];
-	var freqTable=info[1];
-	var keys=Object.keys(freqTable);
-	var wordsUsed={};
-	var str="";
-	var index=getRandomIntInclusive(0,lastWords.length-1);
-	var lastWord=lastWords[index];
-	var seed=null;
-	for(var i=structure.length-1;i>=0;i--){
-		var nextLine=getNextLineNew(structure[i],keys,freqTable,syllDict,wordsUsed,lastWord,seed);
-		var arr=nextLine.split(" ");
-		for(var j=0;j<arr.length;j++){
-			wordsUsed[arr[j]]=true;
-		}
-		str=nextLine+"\n"+str;
-		seed=nextLine.split(" ")[0];
-		if(seed==undefined){
-			seed=lastWords[getRandomIntInclusive(0,lastWords.length-1)];
-		}
-	}
-	console.log(str);
-	
-}
-
-function haikuMarkov(structure){
-	var syllDict=formatData2(cmudictFile);
-	var text=processText('./basho.txt');
-	//console.log(text);
-	var freqTable=generateFrequencyTable(text);
-	var keys=Object.keys(freqTable);
-	//console.log("about to generate");
-	var wordsUsed={};
-	var str="";
-	for(var i=0;i<structure.length;i++){
-		var nextLine=getNextLine(structure[i],keys,freqTable,syllDict,wordsUsed);
-		var arr=nextLine.split(" ");
-		for(var j=0;j<arr.length;j++){
-			wordsUsed[arr[j]]=true;
-		}
-		str=str+nextLine+"\n";
-		//console.log(wordsUsed);
-	}
-	console.log(str);
-	
-
-}
-
+/*Generates chains of words, with probabilities based on the frequency table,
+until the correct syllable account is achieved*/
 function getNextLine(syllables,keys,freqTable,syllDict,wordsUsed){
-	//var firstIndex=getRandomIntInclusive(0,keys.length-1)
-	//var current=keys[firstIndex];	
 	while(true){
 		syllablesLeft=syllables;
 		str="";
@@ -288,18 +178,9 @@ function getNextLine(syllables,keys,freqTable,syllDict,wordsUsed){
 	}
 }
 
-function checkEnd(str){
-	var arr=str.split(" ");
-	var forbidden=["THE","A","OF","ON","BY","FROM","FOR","AND","OR","YET","TO","MY","SHE","WITH","ITS","INTO","BUT","IS"];
-	for(var i=0;i<forbidden.length;i++){
-		if(arr[arr.length-2]==forbidden[i]){
-			//console.log("caught",str);
-			return false;
-		}
-	}
-	return true;
-}
-
+/*Used by both Markov chain approaches; uses the frequency table to generate probabilities 
+for a word to follow the current word;
+selects a new word based on these probabilities*/
 function selectNextWord(current,freqTable,wordsUsed,keys){
 	if(freqTable[current]===undefined){
 		console.log("DEBUG",current); //DO SOMETHING
@@ -331,52 +212,195 @@ function selectNextWord(current,freqTable,wordsUsed,keys){
 		}
 	}
 	var nextWord=successors[counter];
-	if(wordsUsed[nextWord]==true){
-		//console.log("repeat");
+	if(wordsUsed[nextWord]==true){ //Helps avoid overusing words
 		if(Math.random()>.25){
 			if(successors.length!=1){
-				//console.log("1");
 				var newIndex=getRandomIntInclusive(0,successors.length-1)
 				return successors[newIndex];
 			}
-			//console.log("2");
-			//var newIndex=getRandomIntInclusive(0,successors.length-1)
-			//return successors[newIndex];
-
 		}
 	}
 	return nextWord;
 }
 
+/*Used by both Markov chain methods to prevent prepositions etc. from being 
+the last word in a haiku*/
+function checkEnd(str){
+	var arr=str.split(" ");
+	var forbidden=["THE","A","OF","ON","BY","FROM","FOR","AND","OR","YET","TO","MY","SHE","WITH","ITS","INTO","BUT","IS"];
+	for(var i=0;i<forbidden.length;i++){
+		if(arr[arr.length-2]==forbidden[i]){
+			//console.log("caught",str);
+			return false;
+		}
+	}
+	return true;
+}
+
+//MARKOV CHAIN 2
+
+/*Designed specifically to train on other haikus;
+uses word chains to build haiku backwards from 
+the last word*/
+function haikuMarkovNew(structure,text){
+	var syllDict=formatData2(cmudictFile);
+	var text=processTextNew(text);
+	var info=generateFrequencyTableNew(text);
+	var lastWords=info[0];
+	var freqTable=info[1];
+	var keys=Object.keys(freqTable);
+	var wordsUsed={};
+	var str="";
+	var index=getRandomIntInclusive(0,lastWords.length-1);
+	var lastWord=lastWords[index];
+	var seed=null;
+	for(var i=structure.length-1;i>=0;i--){
+		var nextLine=getNextLineNew(structure[i],keys,freqTable,syllDict,wordsUsed,lastWord,seed);
+		var arr=nextLine.split(" ");
+		for(var j=0;j<arr.length;j++){
+			wordsUsed[arr[j]]=true;
+		}
+		str=nextLine+"\n"+str;
+		seed=nextLine.split(" ")[0];
+		if(seed==undefined){
+			seed=lastWords[getRandomIntInclusive(0,lastWords.length-1)];
+		}
+	}
+	console.log(str);
+	
+}
+/*Returns a frequency table tracking mapping a word to the words that appear BEFORE it and their counts;
+also returns a list of appropriate last words in the haiku from the training text*/
+function generateFrequencyTableNew(text){
+	freqTable={};
+	var arr=text.split("\n\n");
+	var lastWords=[];
+	for(var i=0;i<(arr.length-1);i++){
+		//COLLECT LAST WORD
+		var haiku=arr[i].split(" ");
+		var lastWord=haiku[haiku.length-1];
+		var index=haiku.length-1;
+		//console.log(index);
+		var changed=false;
+		while(lastWord==""&&index>0){
+			lastWord=haiku[index];
+			index--;
+			changed=true;
+		}
+		lastWords.push(lastWord);
+		//GENERATE BACKWARDS FREQUENCY TABLE
+		var startIndex=index;
+		if(changed==true){
+			startIndex++;
+		}
+		var chainBack=[];
+		for(var j=startIndex;j>=0;j--){
+			var word=haiku[j];
+			if(word!=""){
+				var words=word.split('\n');
+				for(var k=words.length-1;k>=0;k--){
+					if(words[k]!=""){
+						chainBack.push(words[k]);
+					}
+				}
+			}
+		}	
+		for(var j=0;j<chainBack.length;j++){
+			if(freqTable[chainBack[j]]===undefined){
+	   			freqTable[chainBack[j]]={};
+	   		}
+	   		if(freqTable[chainBack[j]][chainBack[j+1]]===undefined){
+	   			freqTable[chainBack[j]][chainBack[j+1]]=1;
+	  		}
+	  		else{
+	   			freqTable[chainBack[j]][chainBack[j+1]]+=1;		
+	  		}
+	  	}
+	}
+    var toReturn=[lastWords,freqTable];
+	return toReturn;
+}
+/*Converts the text to uppercase and strips most characters*/
+function processTextNew(file){
+	return fs.readFileSync(file).toString().replace(/[^a-zA-Z!' \n]+/g,"").toUpperCase();
+}
+
+/*Generates chains of words, with probabilities based on the frequency table,
+until the correct syllable account is achieved*/
+function getNextLineNew(syllables,keys,freqTable,syllDict,wordsUsed,lastWord,seed){
+	var calls=0;
+	while(true){
+		if(calls>50){ //Lazy way of escaping bugs - should neevr actually get called
+			return getNextLineNew(syllables,keys,freqTable,syllDict,wordsUsed,keys[getRandomIntInclusive(0,keys.length-1)],null);
+		}
+		calls++;
+		var current=lastWord;
+		if(seed!=null){
+			current=selectNextWord(seed,freqTable,wordsUsed,keys);
+		}
+		while((current==undefined)||(syllDict[current]==undefined)){
+			current=keys[getRandomIntInclusive(0,keys.length-1)];
+		}
+		var syllablesUsed=syllDict[current];
+		var syllablesLeft=syllables-syllablesUsed;
+		while(syllablesLeft<0){
+			current=keys[getRandomIntInclusive(0,keys.length-1)];
+			while(syllDict[current]==undefined){
+				current=keys[getRandomIntInclusive(0,keys.length-1)];
+			}
+			syllablesUsed=syllDict[current];
+			syllablesLeft=syllables-syllablesUsed;
+		}
+		var str=current;
+		if(syllablesLeft==0){
+			return str;
+		}
+		var attempts=0;
+		var attemptsAllowed=syllables*5;
+		while(syllablesLeft>0){
+			var nextWord=selectNextWord(current,freqTable,wordsUsed,keys);
+			while(syllDict[nextWord]==undefined){ 
+				nextWord=keys[getRandomIntInclusive(0,keys.length-1)];
+			}
+			if(attempts>=attemptsAllowed){
+				str="";
+				syllablesLeft=syllables;
+				nextWord=keys[getRandomIntInclusive(0,keys.length-1)];
+				while(syllDict[nextWord]==undefined){ 
+					nextWord=keys[getRandomIntInclusive(0,keys.length-1)];
+				}
+				attempts=0;
+			}
+			syllablesUsed=syllDict[nextWord];
+			str=nextWord+" "+str;
+			syllablesLeft-=syllablesUsed;
+			current=nextWord;
+			attempts++;
+			if(syllablesLeft==0&&checkEnd(str)){
+				return str;
+			}
+			if(syllablesLeft<=0){
+				str="";
+				syllablesLeft=syllables;
+				current=keys[getRandomIntInclusive(0,keys.length-1)];
+				while(syllDict[current]==undefined){
+					current=keys[getRandomIntInclusive(0,keys.length-1)];
+				}
+				attempts=0;
+			}
+		}
+	}
+}
 
 function createHaiku(structure){
-	var syllablesArr=formatData(cmudictFile);
-	var complexStructure=[];
-	for(var i=0;i<structure.length;i++){
-		var availableSylls=structure[i];
-		var lineStructure=[];
-		while(availableSylls>0){
-			var syllsInWord=getRandomIntInclusive(1,availableSylls);
-			availableSylls-=syllsInWord;
-			lineStructure.push(syllsInWord);
-		}
-		complexStructure.push(lineStructure);
-	}
-	var poem="\n";
-	for(var i=0;i<complexStructure.length;i++){
-		for(var j=0;j<complexStructure[i].length;j++){
-			var numSylls=complexStructure[i][j];
-			var wordIndex=getRandomIntInclusive(0,syllablesArr[numSylls].length);
-			var word=syllablesArr[numSylls][wordIndex].replace(/[^a-zA-Z!']+/g, ""); //undefined error
-			poem=poem+word+" "
-		}
-		poem+="\n";
-	}
 	console.log("\n[basic method]");
-	console.log(poem);
-	console.log("[extra credit]\n");
-    haikuMarkov(structure);
-    haikuMarkovNew(structure);
+	haikuBasic(structure);
+	console.log("[markov chain 1]\n");
+    haikuMarkov(structure,'./wasteland.txt');
+    console.log("[markov chain 2]\n");
+    for(var i=0;i<3;i++){
+    	haikuMarkovNew(structure,'./basho.txt');
+    }
 }
 
 
